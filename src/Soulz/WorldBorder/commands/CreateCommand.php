e836fa54be95e2459e2db21a53d12abbadf7b47a
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-07-21 22:46:52 -0700

    Update CreateCommand.php

--- src/Soulz/WorldBorder/commands/CreateCommand.php
+++ src/Soulz/WorldBorder/commands/CreateCommand.php
@@ -1,15 +1,27 @@
 <?php
-//
-// namespace Soulz\WorldBorder\commands;
-//
-// use pocketmine\command\{
-//  Command,
-//  CommandSender,
-//  ConsoleCommandSender
-// };
-// use pocketmine\utils\TextFormat;
-// 
-// class CreateCommand extends Command {
-// 
-// }
-//
+
+namespace Soulz\WorldBorder\commands;
+
+use pocketmine\command\{
+ Command,
+ CommandSender
+};
+use pocketmine\utils\TextFormat;
+
+class CreateCommand extends Command {
+
+    public function __construct(){
+        parent::__construct("/wbcreate");
+        $this->setDescription("");
+        $this->setUsage("/wbcreate {border_name}");
+        $this->setPermission("border.create.command");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(!$sender instanceof Player){
+            $sender->sendMessage("You must execute $commandLabel in-game!");
+            return;
+        }
+        // continue later
+    }
+}
