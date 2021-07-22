1b04cd21dea12a17a2c107042c6f90ef2811233b
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-07-21 22:27:53 -0700

    #5

---
+++ src/Soulz/WorldBorder/commands/RemoveCommand.php
@@ -0,0 +1,15 @@
+<?php
+//
+// namespace Soulz\WorldBorder\commands;
+//
+// use pocketmine\command\{
+//  Command,
+//  CommandSender,
+//  ConsoleCommandSender
+// };
+// use pocketmine\utils\TextFormat;
+// 
+// class SetPosCommand extends Command {
+// 
+// }
+//
