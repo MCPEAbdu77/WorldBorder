0598625303be9600b9667951991e5481c1c172d5
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-07-22 22:00:25 -0700

    cleanse

--- src/Soulz/WorldBorder/WorldBorder.php
+++ src/Soulz/WorldBorder/WorldBorder.php
@@ -3,32 +3,20 @@
 namespace Soulz\WorldBorder;
 
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat as TF;
 
-use Soulz\WorldBorder\Utils;
-use Soulz\WorldBorder\EventListener;
-
-class WorldBorder extends PluginBase {
-
-    /** @var self */
-    public static $instance;
+class WorldBorder extends PluginBase implements Listener {
 
     public function onLoad(){
         $this->getLogger()->info("Loading WorldBorder by Soul ✞#9999");
     }
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this),$this);
-        $commands = [
-        new SetPosCommand(),
-        new CreateCommand(),
-        new RemoveCommand()
-        ];
-        $this->getServer()->getCommandMap()->registerAll("BorderCommands", $commands);
 
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
         @mkdir($this->getDataFolder(), 0744, true);
         $this->saveResource('config.yml', false);
         $this->config = new Config($this->getDataFolder().'config.yml', Config::YAML); 
@@ -49,7 +37,7 @@ class WorldBorder extends PluginBase {
 
             if($v2->distance($v1) > $dat[$level->getName()]){
                 $e->setCancelled();
-                $player->sendMessage(Utils::PREFIX.$this->getConfig()->get("border-message").TF::RESET);
+                $player->sendMessage(Utils::RED_PREFIX.$this->getConfig()->get("border-message").TF::RESET);
             }
 
         }
