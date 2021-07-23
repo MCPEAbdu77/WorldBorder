71e070dff254f17ba24904536931572c846c6cfb
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-07-22 21:51:33 -0700

    Update RemoveCommand.php

--- src/Soulz/WorldBorder/commands/RemoveCommand.php
+++ src/Soulz/WorldBorder/commands/RemoveCommand.php
@@ -1,15 +1,20 @@
 <?php
-//
-// namespace Soulz\WorldBorder\commands;
-//
-// use pocketmine\command\{
-//  Command,
-//  CommandSender,
-//  ConsoleCommandSender
-// };
-// use pocketmine\utils\TextFormat;
-// 
-// class SetPosCommand extends Command {
-// 
-// }
-//
+
+namespace Soulz\WorldBorder\commands;
+
+use pocketmine\command\{Command,
+                        CommandSender};
+use pocketmine\level\Position;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+use Soulz\WorldBorder\WorldBorder;
+ 
+class RemoveCommand extends Command {
+
+    public function __construct(){
+        parent::__construct("//wbremove", "Delete a WorldBorder", null, ["//wbdelete"]);
+    }
+
+    
+}
