85f2eee58422d8ae69ea06e43f2c2a0cb63c3ccc
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-07-22 22:04:51 -0700

    create prefix types

--- src/Soulz/WorldBorder/Utils.php
+++ src/Soulz/WorldBorder/Utils.php
@@ -6,9 +6,16 @@ use pocketmine\utils\TextFormat as TF;
 
 class Utils {
 
-    public const PREFIX = TF::GRAY."(".TF::BOLD.TF::RED."!".TF::RESET.TF::GRAY.") ".TF::RESET; // ") " = '(!)border-message' -> '(!) border-message'
+    public const RED_PREFIX = TF::GRAY."[".TF::BOLD.TF::RED."!".TF::RESET.TF::GRAY."] ".TF::RESET; // ") " = '[!]border-message' -> '[!] border-message'
+
+    public const YELLOW_PREFIX = TF::GRAY."[".TF::BOLD.TF::GOL."!".TF::RESET.TF::GRAY."] ".TF::RESET; // ") " = '[!]border-message' -> '[!] border-message'
+
+    public const GREEN_PREFIX = TF::GRAY."[".TF::BOLD.TF::GREEN."!".TF::RESET.TF::GRAY."] ".TF::RESET; // ") " = '[!]border-message' -> '[!] border-message'
 
     public function getPrefix(){
-        return self::PREFIX;
+        return self::RED_PREFIX;
+        return self::YELLOW_PREFIX;
+        return self::GREEN_PREFIX;
   }
+
 }
