7958fedac85d40f27079f69dc40c629d4d84d687
Soulz <84856878+Pinesolz@users.noreply.github.com>
2021-08-31 08:32:42 -0700

    Closes #8 - Prefix no Longer Cancels Message

--- src/Soulz/WorldBorder/Utils.php
+++ src/Soulz/WorldBorder/Utils.php
@@ -2,20 +2,14 @@
 
 namespace Soulz\WorldBorder;
 
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\TextFormat;
 
 class Utils {
 
-    public const RED_PREFIX = TF::GRAY."[".TF::BOLD.TF::RED."!".TF::RESET.TF::GRAY."] ".TF::RESET; // ") " = '[!]border-message' -> '[!] border-message'
+    public const RED_PREFIX = TextFormat::GRAY. "[" . TextFormat::BOLD . TextFormat::RED. "!" . TextFormat::RESET . TextFormat::GRAY . "] " . TextFormat::RESET; // ") " = '[!]border-message' -> '[!] border-message'
 
-    public const YELLOW_PREFIX = TF::GRAY."[".TF::BOLD.TF::GOL."!".TF::RESET.TF::GRAY."] ".TF::RESET; // ") " = '[!]border-message' -> '[!] border-message'
+    public const YELLOW_PREFIX = TextFormat::GRAY. "[" . TextFormat::BOLD . TextFormat::GOLD . "!" . TextFormat::RESET . TextFormat::GRAY . "] " . TextFormat::RESET; // ") " = '[!]border-message' -> '[!] border-message'
 
-    public const GREEN_PREFIX = TF::GRAY."[".TF::BOLD.TF::GREEN."!".TF::RESET.TF::GRAY."] ".TF::RESET; // ") " = '[!]border-message' -> '[!] border-message'
-
-    public function getPrefix(){
-        return self::RED_PREFIX;
-        return self::YELLOW_PREFIX;
-        return self::GREEN_PREFIX;
-  }
+    public const GREEN_PREFIX = TextFormat::GRAY. "[" . TextFormat::BOLD . TextFormat::GREEN . "!" . TextFormat::RESET . TextFormat::GRAY. "] ". TextFormat::RESET; // ") " = '[!]border-message' -> '[!] border-message'
 
 }
